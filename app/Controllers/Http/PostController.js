8a9797d2f3df7a08a7fb533db7b629cfb987594e
Damilola Faseun <damilola.faseun@gmail.com>
2021-07-16 19:21:53 +0100

    Created new post detail field

--- app/Controllers/Http/PostController.js
+++ app/Controllers/Http/PostController.js
@@ -1,17 +1,22 @@
 'use strict'
 
+const Post = use('App/Models/Post');
+
 class PostController {
   async index({ view }) {
-    const posts = [
-      {title: 'Post One', body: 'This is post one'},
-      {title: 'Post Two', body: 'This is post two'},
-      {title: 'Post Three', body: 'This is post three'},
-    ]
+    const posts = await Post.all()
 
     return view.render('posts.index', {
       title: 'latest post',
-      posts: posts,
+      posts: posts.toJSON(),
     });
+  }
+
+  async details({ params, view }) {
+    const post = await Post.find(params.id)
+    return view.render('posts.details', {
+      post: post
+    })
   }
 }
 
