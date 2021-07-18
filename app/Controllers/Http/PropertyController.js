2444a7351c677a41e90de9f3911838d37e98becd
Damilola Faseun <damilola.faseun@gmail.com>
2021-07-18 08:28:15 +0100

    Created controller for property

--- app/Controllers/Http/PropertyController.js
+++ app/Controllers/Http/PropertyController.js
@@ -1,6 +1,9 @@
 'use strict'
 
+const Property = use('App/Models/Property');
+
 class PropertyController {
+
   async index({ view }) {
     const propertiesHeader = [
       { type: 'propertyName', value: 'Property Name' },
@@ -9,26 +12,23 @@ class PropertyController {
       { type: 'rent', value: 'Rent' },
     ]
 
-    const properties = [
-      { propertyName: 'Ocean Wing', value: 100000000, tenant: 8, rent: 5000000 },
-      { propertyName: 'Ultravioliet Royale', value: 120000000, tenant: 12, rent: 7000000 },
-      { propertyName: 'Sky Drive', value: 185000000, tenant: 9, rent: 10000000 },
-      { propertyName: 'Green Royale', value: 76000000, tenant: 4, rent: 2000000 },
-      { propertyName: 'Vanilla Castle', value: 205000000, tenant: 15, rent: 12000000 },
-      { propertyName: 'Tropic Paradise', value: 165000000, tenant: 12, rent: 4000000 },
-      { propertyName: 'Magic Isle', value: 119000000, tenant: 5, rent: 12000000 },
-      { propertyName: 'Eternal Dreams', value: 89000000, tenant: 2, rent: 900000 },
-      { propertyName: 'Rocky Village', value: 145000000, tenant: 20, rent: 750000 },
-      { propertyName: 'Umbrella Verge', value: 30000000, tenant: 7, rent: 1000000 },
-    ]
+    const properties = await Property.all()
+
     return view.render('properties.index', {
       propertiesHeader: propertiesHeader,
-      properties: properties,
+      properties: properties.toJSON(),
     })
   }
 
   async details({ params, view }) {
-    // return view.render(params.id)
+    const property = await Property.find(params.id)    
+    return view.render('properties.details', {
+      property: property,
+    })
+  }
+
+  async new({ view }) {
+    return view.render('properties.new')
   }
 }
 
