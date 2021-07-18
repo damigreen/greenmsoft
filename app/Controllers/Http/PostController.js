dfc8ca14a9f284beeef113761d753d3ab8d9b93b
Damilola Faseun <damilola.faseun@gmail.com>
2021-07-18 18:02:42 +0100

    Added logic to save post

--- app/Controllers/Http/PostController.js
+++ app/Controllers/Http/PostController.js
@@ -22,6 +22,19 @@ class PostController {
   async add({ view }) {
     return view.render('posts.add')
   }
+
+  async store({ request, response, session }) {
+    const post = new Post();
+
+    post.title = request.input('title')
+    post.body = request.input('body')
+
+    await post.save()
+
+    session.flash({ notification: 'Success: Post Added!' })
+
+    return response.redirect('/posts')
+  }
 }
 
 module.exports = PostController
