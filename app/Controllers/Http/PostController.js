731dc44de615bdfe7a17052edfa80096e20147c6
Damilola Faseun <damilola.faseun@gmail.com>
2021-07-19 08:05:23 +0100

    Logic to edit and delete posts

--- app/Controllers/Http/PostController.js
+++ app/Controllers/Http/PostController.js
@@ -48,6 +48,51 @@ class PostController {
 
     return response.redirect('/posts')
   }
+
+  async edit({ params, view }) {
+    const post = await Post.find(params.id)
+    console.log(post.body)
+
+    return view.render('posts.edit', {
+      post: post,
+    })
+  }
+
+  async update({ params, request, response, session }) {
+    const validation = await validate(request.all(), {
+      title: 'required|min:3|max:255',
+      body: 'required|min:3',
+    })
+
+    if (validation.fails()) {
+      console.log('Validation error =>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>')
+      session.withErrors(validation.messages()).flashAll()
+      return response.redirect('back')
+    }
+
+    const post = await Post.find(params.id)
+
+    post.title = request.input('title')
+    post.body = request.input('body')
+
+    await post.save()
+
+    session.flash({ notification: 'Post Updated' })
+
+    return response.redirect('/posts')
+
+
+  }
+
+  async destroy({ params, session, response }) {
+    const post = await Post.find(params.id)
+
+    await post.delete()
+
+    session.flash({ notification: 'Post Deleted' })
+
+    return response.redirect('/posts')
+  }
 }
 
 module.exports = PostController
