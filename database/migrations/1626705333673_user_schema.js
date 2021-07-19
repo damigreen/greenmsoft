72d45dc6fd927e1e75666a61f5aaf6f06b4f866e
Damilola Faseun <damilola.faseun@gmail.com>
2021-07-19 15:47:10 +0100

    Added new field to user

--- database/migrations/1626705333673_user_schema.js
+++ database/migrations/1626705333673_user_schema.js
@@ -5,19 +5,19 @@ const Schema = use('Schema')
 
 class UserSchema extends Schema {
   up () {
-    this.create('users', (table) => {
-      table.increments()
-      table.string('username', 80).notNullable().unique()
-      table.string('email', 254).notNullable().unique()
-      table.string('password', 60).notNullable()
+    this.table('users', (table) => {
+      // alter table
       table.string('confirmation_token')
       table.boolean('is_active').defaultTo(0)
-      table.timestamps()
     })
   }
 
   down () {
-    this.drop('users')
+    this.table('users', (table) => {
+      // reverse alternations
+      table.string('confirmation_token')
+      table.boolean('is_active').defaultTo(0)
+    })
   }
 }
 
