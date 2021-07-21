ae0e7b9288b47424b798fb9f8da48fbb12dfd482
Damilola Faseun <damilola.faseun@gmail.com>
2021-07-21 08:46:44 +0100

    Handle invalid session exception on home route

--- start/hooks.js
+++ start/hooks.js
@@ -3,10 +3,17 @@ const { hooks } = require('@adonisjs/ignitor')
 hooks.after.providersBooted(() => {
   const View = use('View')
   const Env = use('Env')
+  const Exception = use('Exception')
 
   View.global('appUrl', path => {
     const APP_URL = Env.get('APP_URL')
 
     return path ? `${APP_URL}/${path}` : APP_URL
+  })
+
+  // Handle InvalidSessionException
+
+  Exception.handle('InvalidSessionException', (error, { response }) => {
+    return response.redirect('/login') 
   })
 })
